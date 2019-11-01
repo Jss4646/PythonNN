2b720d04e882786db68dfcd5133c0ca2f6d134b1
Jack <jacksandeman@hotmail.co.uk>
2019-11-01 15:41:15 +0000

    Added frontend network support. Groundwork is down

--- web/static/js/network.js
+++ web/static/js/network.js
@@ -1,43 +1,84 @@
+/**
+ * @class
+ * Remove or add layers to and from the network viewport
+ *
+ * @property {object} layers - Holds the html elements in the layers div
+ *
+ * @function {layer}  addLayer
+ * @function {node}   addNode
+ * @function          removeLayer
+ * @function          removeNode
+ *
+ */
+class Network {
 
-const layers = document.querySelector('.layers');
+    constructor() {
+        this.layers = document.querySelector('.layers');
+    }
 
-function network() {
+    /**
+     * Adds a layer to the network viewport
+     * @returns {HTMLDivElement}
+     */
+    addLayer() {
+        let numOfLayers = this.layers.childElementCount;
 
-    this.addLayer = function (addNumOfLayers) {
+        const layer = document.createElement('div');
+        layer.classList.add('layer');
+        layer.id = `layer-${numOfLayers + 1}-viewport`;
 
-        for (let i = 0; i < addNumOfLayers; i++) {
-            let numOfLayers = layers.childElementCount;
+        const title = document.createElement('h2');
+        title.innerText = `Layer ${numOfLayers + 1}`;
+        layer.appendChild(title);
 
-            const layer = document.createElement('div');
-            layer.id = `layer-${numOfLayers + 1}-viewport`;
+        const nodes = document.createElement('div');
+        nodes.classList.add('layer-nodes');
+        layer.appendChild(nodes);
 
-            const title = document.createElement('h2');
-            title.innerText = `Layer ${numOfLayers + 1}`;
-            layer.appendChild(title);
-
-            const nodes = document.createElement('div');
-            nodes.classList.add('layer-nodes');
-            this.addNode(nodes, 1);
-
-            layers.appendChild(layer);
-        }
-
+        this.layers.appendChild(layer);
+        return layer;
     };
 
-    this.addNode = function (layerNodeDiv, addNumOfNodes, numOfLayers) {
+    /**
+     * Adds a node to a given layer
+     * @param layer
+     * @returns {HTMLDivElement}
+     */
+    addNode(layer) {
+        const nodeList = layer.querySelector('.layer-nodes');
+        let numOfNodes = nodeList.childElementCount;
+
         const node = document.createElement('div');
-        node.classList.add('node sigmoid-node no-hover')
-        node.id = ``
+        node.classList.add('node');
+        node.classList.add('sigmoid-node');
+        node.classList.add('no-hover');
+        node.id = `l${layer.id.slice(6)}-node-${numOfNodes + 1}-viewport`;
+
+        const nodeValue = document.createElement('span');
+        nodeValue.innerText = '0.0';
+        node.appendChild(nodeValue);
+
+        nodeList.appendChild(node);
+        return node
     };
 
-    this.removeLayer = function () {
-
+    /**
+     * Removes a given layer
+     */
+    removeLayer(layer) {
+        layer.remove();
     };
 
-    this.removeNode = function () {
-
+    /**
+     * Removes the last node from a layer
+     * @param layer
+     */
+    removeNode(layer) {
+        const nodes = layer.querySelector('.layer-nodes');
+        nodes.childNodes[-1].remove();
     };
 }
 
-const nn = new network();
-nn.addLayer(1);
+// const nn = new Network();
+// nn.addLayers(4);
+// nn.addNodes(nn.layers.childNodes[1],1);
