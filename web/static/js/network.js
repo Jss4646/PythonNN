29779dbe993ffb434f2030bd03fa99cb5f1b1e15
Jack <jacksandeman@hotmail.co.uk>
2019-11-04 00:20:33 +0000

    Node click and hover interactions

--- web/static/js/network.js
+++ web/static/js/network.js
@@ -2,12 +2,12 @@
  * @class
  * Remove or add layers to and from the network viewport
  *
- * @property {object} layers - Holds the html elements in the layers div
+ * @property {object}  layers - Holds the html elements in the layers div
  *
- * @function {layer}  addLayer
- * @function {node}   addNode
- * @function          removeLayer
- * @function          removeNode
+ * @function {layer}   addLayer
+ * @function {node}    addNode
+ * @function {boolean} removeLayer
+ * @function {boolean} removeNode
  *
  */
 class Network {
@@ -19,66 +19,97 @@ class Network {
     /**
      * Adds a layer to the network viewport
      * @returns {HTMLDivElement}
+     * @param numLayersAdded - optional, will add one layer and return it if left blank
+     * @param numNodesInLayer - optional, will add one node to the layer if left blank
      */
-    addLayer() {
-        let numOfLayers = this.layers.childElementCount;
+    addLayer(numLayersAdded = 1, numNodesInLayer = 1) {
+        for (let i = 0; i < numLayersAdded; i++) {
+            let numOfLayers = this.layers.childElementCount;
 
-        const layer = document.createElement('div');
-        layer.classList.add('layer');
-        layer.id = `layer-${numOfLayers + 1}-viewport`;
+            const layer = document.createElement('div');
+            layer.classList.add('layer');
+            layer.id = `layer-${numOfLayers + 1}-viewport`;
 
-        const title = document.createElement('h2');
-        title.innerText = `Layer ${numOfLayers + 1}`;
-        layer.appendChild(title);
+            const title = document.createElement('h2');
+            title.innerText = `Layer ${numOfLayers + 1}`;
+            layer.appendChild(title);
 
-        const nodes = document.createElement('div');
-        nodes.classList.add('layer-nodes');
-        layer.appendChild(nodes);
+            const nodes = document.createElement('div');
+            nodes.classList.add('layer-nodes');
+            layer.appendChild(nodes);
 
-        this.layers.appendChild(layer);
-        return layer;
+            this.addNode(layer, numNodesInLayer);
+
+            this.layers.appendChild(layer);
+
+            if (numLayersAdded === 1) {
+                return layer;
+            }
+        }
     };
 
     /**
      * Adds a node to a given layer
-     * @param layer
+     * @param layer - layer that node will be added to
+     * @param numNodesAdded - optional, will add one node and return it if left blank
      * @returns {HTMLDivElement}
      */
-    addNode(layer) {
-        const nodeList = layer.querySelector('.layer-nodes');
-        let numOfNodes = nodeList.childElementCount;
+    addNode(layer, numNodesAdded = 1) {
+        for (let i = 0; i < numNodesAdded; i++) {
+            const nodeList = layer.querySelector('.layer-nodes');
+            let numOfNodes = nodeList.childElementCount;
 
-        const node = document.createElement('div');
-        node.classList.add('node');
-        node.classList.add('sigmoid-node');
-        node.classList.add('no-hover');
-        node.id = `l${layer.id.slice(6)}-node-${numOfNodes + 1}-viewport`;
+            const node = document.createElement('div');
+            node.classList.add('node');
+            node.classList.add('sigmoid-node');
+            node.classList.add('no-hover');
+            node.id = `l${layer.id.slice(6)}-node-${numOfNodes + 1}-viewport`;
 
-        const nodeValue = document.createElement('span');
-        nodeValue.innerText = '0.0';
-        node.appendChild(nodeValue);
+            addNodeInteraction(node);
 
-        nodeList.appendChild(node);
-        return node
+            const nodeValue = document.createElement('span');
+            nodeValue.innerText = '0.0';
+            node.appendChild(nodeValue);
+
+            nodeList.appendChild(node);
+            if (numNodesAdded === 1) {
+                return node;
+            }
+        }
     };
 
     /**
      * Removes a given layer
+     * @returns {boolean} - True: Success, False: Failure
      */
     removeLayer(layer) {
-        layer.remove();
+        if (this.layers.children.length > 1) {
+            layer.remove();
+            return true;
+        } else {
+            console.log('The network has to contain at least one layer');
+            return false;
+        }
     };
 
     /**
      * Removes the last node from a layer
-     * @param layer
+     * @param layer - layer that node will be removed from
+     * @returns {boolean} - True: Success, False: Failure
      */
     removeNode(layer) {
         const nodes = layer.querySelector('.layer-nodes');
-        nodes.childNodes[-1].remove();
+
+        if (nodes.childNodes.length > 1) {
+            nodes.childNodes[0].remove();
+            return true;
+        } else {
+            console.log('The layer has to contain at least one node');
+            return false;
+        }
     };
 }
 
-// const nn = new Network();
-// nn.addLayers(4);
-// nn.addNodes(nn.layers.childNodes[1],1);
+const nn = new Network();
+nn.addLayer(4, 4);
+
---
+++ web/static/js/nodeInteraction.js
@@ -0,0 +1,26 @@
+/**
+ * Adds hover and click detection to a node
+ * @param node
+ */
+function addNodeInteraction(node) {
+    node.addEventListener("mouseenter", function (selected_node) {
+        selected_node.target.classList.replace('no-hover', 'node-hover')
+    });
+
+    node.addEventListener("mouseleave", function (selected_node) {
+        selected_node.target.classList.replace('node-hover', 'no-hover')
+    });
+
+    node.addEventListener("click", function (selected_node) {
+
+        selected_node.target.classList.replace('node-hover', 'node-selected');
+
+        const nodes = document.querySelectorAll('.node');
+
+        nodes.forEach(function (node) {
+            if (node !== selected_node.target && node.classList.contains('node-selected')) {
+                node.classList.replace('node-selected', 'no-hover');
+            }
+        })
+    });
+}
